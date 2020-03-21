6a78df6d1ead1f2f1b40e6562def9df22c5fc716
bozar42 <5583771+Bozar@users.noreply.github.com>
2020-03-21 16:18:16 +0800

    Initialize all sprites in the demo.

--- scene/main/InitWorld.gd
+++ scene/main/InitWorld.gd
@@ -1,9 +1,13 @@
 extends Node2D
 
 
+const Player := preload("res://sprite/PC.tscn")
+const Dwarf := preload("res://sprite/Dwarf.tscn")
+const Floor := preload("res://sprite/Floor.tscn")
 const Wall := preload("res://sprite/Wall.tscn")
 const ArrowX := preload("res://sprite/ArrowX.tscn")
 const ArrowY := preload("res://sprite/ArrowY.tscn")
+
 const ConvertCoord := preload("res://library/ConvertCoord.gd")
 
 const MAX_X: int = 21
@@ -14,30 +18,43 @@ var _get_coord: ConvertCoord = ConvertCoord.new()
 
 
 func _ready() -> void:
-	_init_wall_block()
+	_init_floor()
+	_init_wall()
+	_init_PC()
+	_init_dwarf()
 	_init_indicator()
 
 
-func _init_wall_block() -> void:
-	var wall: Sprite
+func _init_dwarf() -> void:
+	for i in [[4, 5], [15, 6], [12, 13]]:
+		_create_sprite(Dwarf, i[0], i[1])
 
+
+func _init_PC() -> void:
+	_create_sprite(Player, 0, 0)
+
+
+func _init_floor() -> void:
 	for i in range(MAX_X):
 		for j in range(MAX_Y):
-			if (i != 0) and (j != 0):
-				continue
+			_create_sprite(Floor, i, j)
 
-			wall = Wall.instance() as Sprite
-			wall.position = _get_coord.index_to_vector(i, j)
-			add_child(wall)
+
+func _init_wall() -> void:
+	for i in range(9, 13):
+		for j in range(5, 9):
+			_create_sprite(Wall, i, j)
 
 
 func _init_indicator() -> void:
-	var indicator: Sprite
+	_create_sprite(ArrowX, 0, 12, -ARROW_MARGIN)
+	_create_sprite(ArrowY, 5, 0, 0, -ARROW_MARGIN)
 
-	indicator = ArrowX.instance() as Sprite
-	indicator.position = _get_coord.index_to_vector(0, 12, -ARROW_MARGIN)
-	add_child(indicator)
 
-	indicator = ArrowY.instance() as Sprite
-	indicator.position = _get_coord.index_to_vector(5, 0, 0, -ARROW_MARGIN)
-	add_child(indicator)
+func _create_sprite(prefab: PackedScene, x: int, y: int,
+		x_offset: int = 0, y_offset: int = 0) -> void:
+	var new_sprite: Sprite
+
+	new_sprite = prefab.instance() as Sprite
+	new_sprite.position = _get_coord.index_to_vector(x, y, x_offset, y_offset)
+	add_child(new_sprite)
