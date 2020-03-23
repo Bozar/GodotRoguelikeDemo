2e6bda7fb4d16d5a35cdcfd0381c658d779c4643
bozar42 <5583771+Bozar@users.noreply.github.com>
2020-03-23 21:27:18 +0800

    Move PC in respond to keyboard inputs.

--- scene/pc_act/PCMove.gd
+++ scene/pc_act/PCMove.gd
@@ -1,18 +1,30 @@
 extends Node2D
 
 
+const ConvertCoord := preload("res://library/ConvertCoord.gd")
+
+var _get_coord: ConvertCoord = ConvertCoord.new()
 var _pc: Sprite
 
 
-func _unhandled_input(event):
+func _unhandled_input(event) -> void:
+	if _pc == null:
+		return
+
+	var pos: Array = _get_coord.vector_to_array(_pc.position)
+	var x: int = pos[0]
+	var y: int = pos[1]
+
 	if event.is_action_pressed("move_left"):
-		print("move left")
+		x -= 1
 	elif event.is_action_pressed("move_right"):
-		print("move right")
+		x += 1
 	elif event.is_action_pressed("move_up"):
-		print("move up")
+		y -= 1
 	elif event.is_action_pressed("move_down"):
-		print("move down")
+		y += 1
+
+	_pc.position = _get_coord.index_to_vector(x, y)
 
 
 func _on_InitWorld_sprite_created(new_sprite: Sprite) -> void:
