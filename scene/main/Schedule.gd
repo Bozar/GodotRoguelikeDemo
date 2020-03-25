557c022227f8354ea9d4dec5740b0dd5789c3e98
bozar42 <5583771+Bozar@users.noreply.github.com>
2020-03-25 16:36:12 +0800

    Add end_turn() to Schedule.gd.

--- scene/main/Schedule.gd
+++ scene/main/Schedule.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 
 var _actors: Array = [null]
+var _pointer: int = 0
 
 
 func _on_InitWorld_sprite_created(new_sprite: Sprite) -> void:
@@ -9,3 +10,20 @@ func _on_InitWorld_sprite_created(new_sprite: Sprite) -> void:
 		_actors[0] = new_sprite
 	elif new_sprite.is_in_group("dwarf"):
 		_actors.append(new_sprite)
+
+
+func end_turn() -> void:
+	_goto_next()
+	print(_get_current().name)
+	# Emit a signal.
+
+
+func _get_current() -> Sprite:
+	return _actors[_pointer] as Sprite
+
+
+func _goto_next() -> void:
+	_pointer += 1
+
+	if _pointer > len(_actors) - 1:
+		_pointer = 0
