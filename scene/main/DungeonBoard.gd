ad07b23acb19a757e0a62c3a45184b05686249c0
bozar42 <5583771+Bozar@users.noreply.github.com>
2020-03-28 22:16:19 +0800

    Fix coding style.

--- scene/main/DungeonBoard.gd
+++ scene/main/DungeonBoard.gd
@@ -12,21 +12,6 @@ func _ready() -> void:
 	_init_dict()
 
 
-func _on_InitWorld_sprite_created(new_sprite: Sprite) -> void:
-	var pos: Array
-	var group: String
-
-	if new_sprite.is_in_group(_group_name.DWARF):
-		group = _group_name.DWARF
-	elif new_sprite.is_in_group(_group_name.WALL):
-		group = _group_name.WALL
-	else:
-		return
-
-	pos = _coord.vector_to_array(new_sprite.position)
-	_sprite_dict[group][pos[0]][pos[1]] = new_sprite
-
-
 func is_inside_dungeon(x: int, y: int) -> bool:
 	return (x > -1) and (x < _dungeon.MAX_X) \
 			and (y > -1) and (y < _dungeon.MAX_Y)
@@ -40,6 +25,21 @@ func get_sprite(group_name: String, x: int, y: int) -> Sprite:
 	if not is_inside_dungeon(x, y):
 		return null
 	return _sprite_dict[group_name][x][y]
+
+
+func _on_InitWorld_sprite_created(new_sprite: Sprite) -> void:
+	var pos: Array
+	var group: String
+
+	if new_sprite.is_in_group(_group_name.DWARF):
+		group = _group_name.DWARF
+	elif new_sprite.is_in_group(_group_name.WALL):
+		group = _group_name.WALL
+	else:
+		return
+
+	pos = _coord.vector_to_array(new_sprite.position)
+	_sprite_dict[group][pos[0]][pos[1]] = new_sprite
 
 
 func _init_dict() -> void:
--- scene/main/Schedule.gd
+++ scene/main/Schedule.gd
@@ -10,13 +10,6 @@ var _pointer: int = 0
 var _counter: int = 0
 
 
-func _on_InitWorld_sprite_created(new_sprite: Sprite) -> void:
-	if new_sprite.is_in_group(_group_name.PC):
-		_actors[0] = new_sprite
-	elif new_sprite.is_in_group(_group_name.DWARF):
-		_actors.append(new_sprite)
-
-
 func end_turn() -> void:
 	_goto_next()
 
@@ -26,6 +19,13 @@ func end_turn() -> void:
 		print("Turn: {0}".format([_counter]))
 
 	emit_signal("turn_started", _get_current())
+
+
+func _on_InitWorld_sprite_created(new_sprite: Sprite) -> void:
+	if new_sprite.is_in_group(_group_name.PC):
+		_actors[0] = new_sprite
+	elif new_sprite.is_in_group(_group_name.DWARF):
+		_actors.append(new_sprite)
 
 
 func _get_current() -> Sprite:
