09084b3c4713bf87de4a8cbe98eaf2a0a8576d9b
bozar42 <5583771+Bozar@users.noreply.github.com>
2020-04-03 14:15:33 +0800

    Rename constants in MainScene.gd.

--- scene/main/MainScene.gd
+++ scene/main/MainScene.gd
@@ -1,69 +1,69 @@
 extends "res://library/RootNodeTemplate.gd"
 
 
-const NODE_INIT: String = "InitWorld"
-const NODE_PC_MOVE: String = "PCMove"
-const NODE_PC_ATTACK: String = "PCMove/PCAttack"
-const NODE_NPC: String = "EnemyAI"
-const NODE_SCHEDULE: String = "Schedule"
-const NODE_DUNGEON: String = "DungeonBoard"
-const NODE_REMOVE: String = "RemoveObject"
-const NODE_SIDEBAR: String = "MainGUI/MainHBox/SidebarVBox"
-const NODE_MODELINE: String = "MainGUI/MainHBox/Modeline"
+const INIT: String = "InitWorld"
+const PC_MOVE: String = "PCMove"
+const PC_ATTACK: String = "PCMove/PCAttack"
+const NPC: String = "EnemyAI"
+const SCHEDULE: String = "Schedule"
+const DUNGEON: String = "DungeonBoard"
+const REMOVE: String = "RemoveObject"
+const SIDEBAR: String = "MainGUI/MainHBox/SidebarVBox"
+const MODELINE: String = "MainGUI/MainHBox/Modeline"
 
 const SIGNAL_BIND: Array = [
 	[
 		"sprite_created", "_on_InitWorld_sprite_created",
-		NODE_INIT,
-		NODE_PC_MOVE, NODE_NPC, NODE_SCHEDULE, NODE_DUNGEON,
+		INIT,
+		PC_MOVE, NPC, SCHEDULE, DUNGEON,
 	],
 	[
 		"turn_started", "_on_Schedule_turn_started",
-		NODE_SCHEDULE,
-		NODE_PC_MOVE, NODE_NPC, NODE_SIDEBAR, NODE_MODELINE
+		SCHEDULE,
+		PC_MOVE, NPC, SIDEBAR, MODELINE
 	],
 	[
 		"turn_ended", "_on_Schedule_turn_ended",
-		NODE_SCHEDULE,
-		NODE_MODELINE,
+		SCHEDULE,
+		MODELINE,
 	],
 	[
 		"sprite_removed", "_on_RemoveObject_sprite_removed",
-		NODE_REMOVE,
-		NODE_DUNGEON, NODE_SCHEDULE,
+		REMOVE,
+		DUNGEON, SCHEDULE,
 	],
 	[
 		"enemy_warned", "_on_EnemyAI_enemy_warned",
-		NODE_NPC,
-		NODE_MODELINE,
+		NPC,
+		MODELINE,
 	],
 	[
 		"pc_moved", "_on_PCMove_pc_moved",
-		NODE_PC_MOVE,
-		NODE_MODELINE,
+		PC_MOVE,
+		MODELINE,
 	],
 	[
 		"pc_attacked", "_on_PCAttack_pc_attacked",
-		NODE_PC_ATTACK,
-		NODE_MODELINE,
+		PC_ATTACK,
+		MODELINE,
 	],
 ]
 
 const NODE_REF: Array = [
 	[
 		"_ref_DungeonBoard",
-		NODE_DUNGEON,
-		NODE_PC_MOVE, NODE_PC_ATTACK, NODE_REMOVE,
+		DUNGEON,
+		PC_MOVE, PC_ATTACK, REMOVE,
 	],
 	[
 		"_ref_Schedule",
-		NODE_SCHEDULE,
-		NODE_PC_MOVE, NODE_NPC, NODE_PC_ATTACK,
+		SCHEDULE,
+		PC_MOVE, NPC, PC_ATTACK,
 	],
 	[
 		"_ref_RemoveObject",
-		NODE_REMOVE,
-		NODE_PC_ATTACK,
+		REMOVE,
+		PC_ATTACK,
 	],
 ]
 
