0a484c8ec71869a3522bc836c984050a92fc9a67
bozar42 <5583771+Bozar@users.noreply.github.com>
2020-04-03 19:02:56 +0800

    Minor fix.

--- scene/main/ReloadGame.gd
+++ scene/main/ReloadGame.gd
@@ -2,14 +2,14 @@ extends Node2D
 
 
 const PATH_TO_MAIN: String = "res://scene/main/MainScene.tscn"
-const ROOT_NODE: String = "/root"
 
 
 func reload() -> void:
 	var new_scene: Node2D = load(PATH_TO_MAIN).instance()
 	var old_scene: Node2D = get_tree().current_scene
 
-	get_node(ROOT_NODE).add_child(new_scene)
+	get_tree().root.add_child(new_scene)
 	get_tree().current_scene = new_scene
-	get_node(ROOT_NODE).remove_child(old_scene)
+
+	get_tree().root.remove_child(old_scene)
 	old_scene.queue_free()
